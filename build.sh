a8d3191eff349700e7fca71b724a2253f45cb999
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2021-12-25 12:07:01 +0900

    なんか速いらしいコンパイルオプション

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 set -xe
 
-/home/kounoike/llvm/clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04/bin/clang++ -o yokai -Ofast -march=native -ffast-math yokai.cpp
+/home/kounoike/llvm/clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04/bin/clang++ -o yokai -Ofast -mllvm -polly -march=native -ffast-math yokai.cpp
