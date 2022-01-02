1198468b9a6381756065b6a4222bb2ed131b73e7
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2022-01-02 20:22:21 +0900

    ちょっとビルドスクリプトを書き換え

--- build_bw_fw.sh
+++ build_bw_fw.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-clang++ -std=c++20 -o yokai_bw -Ofast -march=znver3 -mtune=znver3 yokai_bw.cpp
-# clang++ -Wall -std=c++20 -o yokai_bw -O0 -g yokai_bw.cpp
+ARCH=znver2
 
-clang++ -std=c++20 -o yokai_fw -Ofast -march=znver3 -mtune=znver3 yokai_fw.cpp
-# clang++ -Wall -std=c++20 -o yokai_fw -O0 -g yokai_fw.cpp
+clang++ -std=c++20 -o yokai_bw -Ofast -march=${ARCH} -mtune=${ARCH} yokai_bw.cpp -pthread
+# clang++ -Wall -std=c++20 -o yokai_bw -O0 -g yokai_bw.cpp -pthread
+
+clang++ -std=c++20 -o yokai_fw -Ofast -march=${ARCH} -mtune=${ARCH} yokai_fw.cpp -pthread
+# clang++ -Wall -std=c++20 -o yokai_fw -O0 -g yokai_fw.cpp -pthread
